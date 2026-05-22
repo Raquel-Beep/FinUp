0b1d7f9832e3ea70feaf717e3bb1a661b4618f20
Crystyan Marins <crystyanmarins@gmail.com>
2026-05-22 02:03:01 -0300

    Update GROQ API key to placeholder
    Replaced the GROQ API key with a placeholder.

--- services/groq.js
+++ services/groq.js
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import "dotenv/config";
 
 const client = new OpenAI({
-  apiKey: process.env.GROQ_API_KEY,
+  apiKey: "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 
   baseURL: "https://api.groq.com/openai/v1",
 });
