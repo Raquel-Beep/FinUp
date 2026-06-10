eff104fb0d177f6dd3ab48ddceb90be688d58df1
luanaairosa <luanaairosadd@gmail.com>
2026-06-09 23:12:36 -0300

    Projeto alterado
    Código voltando a pegar Tela Admin e MovimentaçãoScreen, com armazenamento local em tela de despesas e investimentos.

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import 'react-native-reanimated';
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -10,15 +11,16 @@ import DespesasScreen from "./screens/DespesasScreen";
 import InvestimentosScreen from "./screens/InvestimentosScreen";
 import ProScreen from "./screens/ProScreen";
 import MovimentacaoScreen from "./screens/MovimentacaoScreen";
+import AdminScreen from "./screens/AdminScreen";
+import AdicionarDespesaScreen from "./screens/AdicionarDespesaScreen";
+import AdicionarInvestimentoScreen from "./screens/AdicionarInvestimentoScreen";
+import CardRegistrationScreen from "./screens/CardRegistrationScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-
   return (
-
     <NavigationContainer>
-
       <Stack.Navigator initialRouteName="HomeInicial">
 
         <Stack.Screen
@@ -59,11 +61,34 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={MovimentacaoScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Admin"
+          component={AdminScreen}
+        />
+
+        <Stack.Screen
+          name="AdicionarDespesa"
+          component={AdicionarDespesaScreen}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="AdicionarInvestimento"
+          component={AdicionarInvestimentoScreen}
+          options={{ headerShown: false }}
+        />
+
+        {/* NOVA TELA DE REGISTRO DE CARTÃO */}
+        <Stack.Screen
+          name="CardRegistration"
+          component={CardRegistrationScreen}
+          options={{ headerShown: false }}
         />
 
       </Stack.Navigator>
-
     </NavigationContainer>
-
   );
 }
